bf2c42c748aab9b877789fb4a2c7a34a89593950
JamesCordova <jcordovas@unsa.edu.pe>
2023-07-01 21:32:40 -0500

    Manage Corsheaders and its requirements

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    # Apps
     'apps.forum',
+    # Python packages
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # corsheaders
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+    'http://localhost:5173',
 ]
 
 ROOT_URLCONF = 'core.urls'
