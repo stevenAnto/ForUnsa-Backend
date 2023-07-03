32d769fc97a6ea8a968a9bc14421760da14358ec
JamesCordova <jcordovas@unsa.edu.pe>
2023-07-03 11:57:34 -0500

    Validator specified
    Validator of weight specified for X MB's, and added some ocurrences in .gitignore

--- apps/forum/models/custom_user.py
+++ apps/forum/models/custom_user.py
@@ -1,20 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from apps.forum.validators import MaxWeightValidator
 from . import Base, School
 
 class CustomUser(User, Base):
     # id, name, last_name, email, nickname, is_admin, is_staff included on User
-    current_school = models.ForeignKey(School, on_delete=models.SET(School.get_default_school)) # School of the user e.g. System Engineering
-    biography = models.CharField(max_length=255, blank=True) # description of user
-    img = models.ImageField(upload_to='profiles', default='default.jpg')
-    is_featured = models.BooleanField(default=False) # user is featured
-    semester = models.CharField(max_length=32, default='not defined') # inital semester
-    slug = models.SlugField(max_length=64, unique=True, editable=False) # slug for links
-    
+    current_school = models.ForeignKey(School, on_delete=models.SET(School.get_default_school))  # School of the user e.g. System Engineering
+    biography = models.CharField(max_length=255, blank=True)  # description of user
+    img = models.ImageField(upload_to='profiles', default='default.jpg', validators=[MaxWeightValidator(10)])
+    is_featured = models.BooleanField(default=False)  # user is featured
+    semester = models.CharField(max_length=32, default='not defined')  # inital semester
+    slug = models.SlugField(max_length=64, unique=True,editable=False)  # slug for links
+
     def get_deleted_user():
         return CustomUser.objects.get_or_create(username="Deleted", )[0]
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.username)
         super(User, self).save(*args, **kwargs)
