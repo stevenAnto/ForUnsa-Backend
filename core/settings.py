d42d9a816fd7f3ccb52a6ee26ec86d2eed76c881
stv <79427465+stevenAnto@users.noreply.github.com>
2023-08-13 11:39:28 -0500

    Update settings.py on cliente url

--- core/settings.py
+++ core/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
         'https://stevenanto.github.io/',
-        'https://for-unsa-front.vercel.app/',
+        'https://for-unsa-front.vercel.app',
         'http://localhost:4200',
         'http://localhost:5173',
         ]
